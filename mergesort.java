b3c120b5303ef9b5fd646ee016845d204b593dca
nakuram <mnnkr@users.noreply.github.com>
2019-08-02 00:14:20 -0700

    started mergeSort

--- mergesort.java
+++ mergesort.java
@@ -1,1 +1,14 @@
-adgadg
+Class mergeSort {
+
+
+    public static void main(String[] args) {
+        int[] array = new int[]{5,7, 8, 31, 4, 65, 7, 2, 4, 73};
+        
+    }
+
+    public static void mergeSort(int[] array) {
+
+
+    }
+
+}
